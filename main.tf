3023d74af68fc09793c8ad5461ca53cf4918ef41
joechapman-vp <joseph.chapman@vantagepointglobal.com>
2023-01-18 14:24:20 +0000

    Move terraform config to terraform.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-# Define our Terraform settings
-terraform {
-  # We only require the AWS provider
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  # The version of Terraform
-  # required to run this code
-  required_version = ">= 1.2.0"
-}
-
-# Configure the AWS provider with the 
-# region declared in our variables.tf
-provider "aws" {
-  region = var.aws_region
-}
-
 # Create a data object that contains
 # all availability zones in the 
 # region with a state of "available"
